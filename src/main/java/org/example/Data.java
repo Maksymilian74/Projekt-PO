c34eb583f3777762affe1ad2605f815a6ed3e795
Maksymilian74 <273007@student.pwr.edu.pl>
2023-05-27 21:50:14 +0200

    Dodanie klasy Forest + modyfikacja Data

--- src/main/java/org/example/Data.java
+++ src/main/java/org/example/Data.java
@@ -9,7 +9,7 @@ public class Data {
     public ArrayList<Mushroom> ListMushroom;
     public ArrayList<ForestFruits> ListFruits;
 
-    public Data(ArrayList<Wolf> ListWolf, ArrayList<Character> ListCharacter, ArrayList<Trees> ListTrees, ArrayList<Mushroom> ListMushroom, ArrayList<ForestFruits> ListFruits) {
+    public Data() {
         ListWolf = new ArrayList<>();
         ListCharacter = new ArrayList<>();
         ListTrees = new ArrayList<>();
