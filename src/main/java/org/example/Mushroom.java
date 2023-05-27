a0910af8d9ba67aec4083bbc190c50c6bb35227f
Maksymilian74 <273007@student.pwr.edu.pl>
2023-05-27 19:37:54 +0200

    Dodanie klasy Mushroom

---
+++ src/main/java/org/example/Mushroom.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Mushroom extends Object{
+    private int id;
+    private boolean IfCollected;
+    private String name;
+    public Mushroom(int position_x, int position_y, int id, boolean IfCollected, String name) {
+        super(position_x, position_y);
+        this.id = id;
+        this.IfCollected = IfCollected;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getIfCollected() {
+        return IfCollected;
+    }
+
+    public void setIfCollected() {
+        this.IfCollected = IfCollected;
+    }
+}
