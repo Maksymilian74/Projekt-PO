fca0a146b67c94528b5dbf80d81ee8e4aee82934
Maksymilian74 <273007@student.pwr.edu.pl>
2023-05-27 21:50:37 +0200

    Dodanie klasy Forest

---
+++ src/main/java/org/example/Forest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Forest {
+    private int size_x;
+    private int size_y;
+    private Data Data;
+
+    public Forest(int size_x, int size_y, Data Data) {
+        this.size_x = size_x;
+        this.size_y = size_y;
+        Data = new Data();
+    }
+
+    /*public void generateMap() {
+
+    }*/
+}
