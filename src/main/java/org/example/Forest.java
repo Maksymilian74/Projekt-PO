878d75e0039a50b42643c42e0ef3cfb5bba43300
Nurek54 <g_sienkiewicz@o2.pl>
2023-05-28 18:08:23 +0200

    Modyfikacja klasy Forest: -użytkownik może podawac szerokość i wysokość planszy lasu -wstępne generowanie lasu na konsoli, literka L ma oznaczać  pole lasu Modyfikacja klasy Main: - dodanie menu w któym program umożliwia użytkownikowi podanie rozmiaru lasów a następnie wygenerowanie go i wyśiwetlenie na konsoli

--- src/main/java/org/example/Forest.java
+++ src/main/java/org/example/Forest.java
@@ -1,16 +1,21 @@
 package org.example;
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Forest {
     private int size_x;
     private int size_y;
     private Data Data;
-
+    private List<List<String>> forestMap;
     public Forest() {
         this.size_x = size_x;
         this.size_y = size_y;
         Data = new Data();
+        this.forestMap = new ArrayList<>();
+
+
+
     }
 
     public int getSize_x() {
@@ -31,15 +36,46 @@ public class Forest {
 
     public void getForestSize() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj wysokość lasu: ");
-        int height = scanner.nextInt();
-        System.out.println("Podaj szerokość lasu: ");
-        int width = scanner.nextInt();
+        int width, height;
+
+        while (true) {
+            System.out.println("Podaj szerokość lasu (do 50): ");
+            width = scanner.nextInt();
+            if (width <= 50)
+                break;
+            else
+                System.out.println("Nieprawidłowa szerokość. Podaj wartość do 50.");
+        }
+
+        while (true) {
+            System.out.println("Podaj wysokość lasu (do 50): ");
+            height = scanner.nextInt();
+            if (height <= 50)
+                break;
+            else
+                System.out.println("Nieprawidłowa wysokość. Podaj wartość do 50.");
+        }
 
         setSize_x(width);
         setSize_y(height);
     }
-    /*public void generateMap() {
+    public void generateForest() {
+        forestMap.clear();
 
-    }*/
+        for (int y = 0; y < size_y; y++) {
+            List<String> row = new ArrayList<>();
+            for (int x = 0; x < size_x; x++) {
+                row.add("L");
+            }
+            forestMap.add(row);
+        }
+    }
+    public void displayForest() {
+        for (List<String> row : forestMap) {
+            for (String cell : row) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Simulation.runSimulation();
+        Scanner scanner = new Scanner(System.in);
+        Forest forest = new Forest();
+
+        while (true) {
+            System.out.println("\nWybierz opcję:");
+            System.out.println("1. Wygeneruj nową mapę lasu");
+            System.out.println("2. Wyświetl aktualną mapę lasu");
+            System.out.println("0. Zakończ program");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    forest.getForestSize();
+                    forest.generateForest();
+                    System.out.println("Mapa lasu została wygenerowana.");
+                    break;
+                case 2:
+                    System.out.println("Aktualna mapa lasu:");
+                    forest.displayForest();
+                    break;
+                case 0:
+                    System.out.println("Niedozobaczenia!! Elo xD");
+                    return;
+                default:
+                    System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
+            }
+        }
     }
 }
