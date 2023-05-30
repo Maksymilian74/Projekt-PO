6fc5299aa662c51d3a82d07a37a45f64951236e3
Nurek54 <g_sienkiewicz@o2.pl>
2023-05-30 12:05:39 +0200

    Modyfikacja klasy Simulation

--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -8,6 +8,7 @@ public class Simulation {
     private Forest forest;
     private int characterX;
     private int characterY;
+    private String previousCell;
 
     public Simulation(int round, int characterMove, Forest forest) {
         this.round = round;
@@ -15,6 +16,7 @@ public class Simulation {
         this.forest = forest;
         this.characterX = -1;
         this.characterY = -1;
+        this.previousCell = null;
     }
 
     public int getRound() {
@@ -33,6 +35,7 @@ public class Simulation {
         if (x >= 0 && x < forest.getSize_x() && y >= 0 && y < forest.getSize_y()) {
             characterX = x;
             characterY = y;
+            previousCell = forest.getForestMap().get(y).get(x);
             forest.getForestMap().get(y).set(x, "*");
             System.out.println("Postać została umieszczona na pozycji (" + x + "," + y + ").");
         } else {
@@ -70,13 +73,26 @@ public class Simulation {
 
     private void moveCharacter(int newX, int newY) {
         if (newX >= 0 && newX < forest.getSize_x() && newY >= 0 && newY < forest.getSize_y()) {
-            forest.getForestMap().get(characterY).set(characterX, "L");
+            String newCell = forest.getForestMap().get(newY).get(newX);
+
+            if (newCell.equals("D") || newCell.equals("K")) {
+                System.out.println("Nie możesz przesunąć postaci na to pole.");
+                return;
+            }
+
+            forest.getForestMap().get(characterY).set(characterX, previousCell);
+            previousCell = newCell;
             characterX = newX;
             characterY = newY;
             forest.getForestMap().get(newY).set(newX, "*");
+
+            if (newCell.equals("G")) {
+                System.out.println("Znalazłeś grzyba!");
+            }
+
             System.out.println("Postać przesunięta na pozycję (" + newX + "," + newY + ").");
         } else {
-            System.out.println("Nieprawidłowy ruch!!!!!!!!!!!!!!!!!!!");
+            System.out.println("Nieprawidłowy ruch!");
         }
     }
 }
