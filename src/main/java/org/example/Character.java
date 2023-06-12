e70cbff44de7ecb416c1b19e979162d562ea8449
Maksymilian74 <273007@student.pwr.edu.pl>
2023-06-12 22:52:41 +0200

    Dodanie zbierania do koszyka

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -1,13 +1,14 @@
 package org.example;
 
-import javax.swing.JLabel;
-import javax.swing.text.DefaultStyledDocument;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Character {
     private int position_x;
     private int position_y;
     private String previousCell;
     private Forest forest;
+    private HashMap<String, Integer> Basket;
 
     public Character(Forest forest) {
         this.forest = forest;
@@ -15,12 +16,18 @@ public class Character {
         position_y = 0;
         previousCell = "L";
         forest.setCell(position_x, position_y, "☺");
+        Basket = new HashMap<>();
+    }
+
+    public Map<String,Integer> getBasket() {
+        return Basket;
     }
 
     public void moveUp() {
         if (position_x > 0) {
             String targetCell = forest.getCell(position_x - 1, position_y);
             if (!targetCell.equals("D") && !targetCell.equals("R") && !targetCell.equals("S")) {
+                collectItem(targetCell);
                 handleCell(targetCell, position_x - 1, position_y);
             } else {
                 forest.setCell(position_x, position_y, "\uD83D\uDE21");
@@ -35,6 +42,7 @@ public class Character {
         if (position_x < forest.getSize_x() - 1) {
             String targetCell = forest.getCell(position_x + 1, position_y);
             if (!targetCell.equals("D") && !targetCell.equals("R") && !targetCell.equals("S")) {
+                collectItem(targetCell);
                 handleCell(targetCell, position_x + 1, position_y);
             } else {
                 forest.setCell(position_x, position_y, "\uD83D\uDE21");
@@ -46,6 +54,7 @@ public class Character {
         if (position_y > 0) {
             String targetCell = forest.getCell(position_x, position_y - 1);
             if (!targetCell.equals("D") && !targetCell.equals("R") && !targetCell.equals("S")) {
+                collectItem(targetCell);
                 handleCell(targetCell, position_x, position_y - 1);
             } else {
                 forest.setCell(position_x, position_y, "\uD83D\uDE21");
@@ -57,11 +66,66 @@ public class Character {
         if (position_y < forest.getSize_y() - 1) {
             String targetCell = forest.getCell(position_x, position_y + 1);
             if (!targetCell.equals("D") && !targetCell.equals("R") && !targetCell.equals("S")) {
+                collectItem(targetCell);
                 handleCell(targetCell, position_x, position_y + 1);
             } else {
                 forest.setCell(position_x, position_y, "\uD83D\uDE21");
             }
         }
+    }
+
+    private void collectItem(String targetCell) {
+        String nazwa="";
+        switch (targetCell) {
+            case "B":
+                nazwa = "Borówki";
+                break;
+            case "J":
+                nazwa = "Jagody";
+                break;
+            case "E":
+                nazwa = "Jeżyny";
+                break;
+            case "A":
+                nazwa = "Maliny";
+                break;
+            case "C":
+                nazwa = "Muchomor Czerwony";
+                break;
+            case "T":
+                nazwa = "Muchomor Sromotnikowy";
+                break;
+            case "P":
+                nazwa = "Podgrzybek";
+                break;
+            case "O":
+                nazwa = "Borowik";
+                break;
+            case "M":
+                nazwa = "Maitake";
+                break;
+            case "I":
+                nazwa = "Pieprznik";
+                break;
+            default:
+                break;
+        }
+
+        if(!targetCell.equals("L")) {
+            if (Basket.containsKey(nazwa)) {
+                int count = Basket.get(nazwa);
+                Basket.put(nazwa, count + 1);
+            } else {
+                Basket.put(nazwa, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : Basket.entrySet()) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+            System.out.println(word + ": " + count);
+        }
+        System.out.println();
     }
 
     private void handleCell(String targetCell, int new_x, int new_y) {
--- src/main/java/org/example/Data.java
+++ src/main/java/org/example/Data.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 
 public class Data {
     public static ArrayList<Wolf> ListWolf;
-    public ArrayList<Character> ListCharacter;
     public static ArrayList<Trees> ListTrees;
     public static ArrayList<Mushroom> ListMushroom;
     public static ArrayList<ForestFruits> ListFruits;
 
     public Data() {
         ListWolf = new ArrayList<Wolf>();
-        ListCharacter = new ArrayList<Character>();
         ListTrees = new ArrayList<Trees>();
         ListMushroom = new ArrayList<Mushroom>();
         ListFruits = new ArrayList<ForestFruits>();
