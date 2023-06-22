3dc15cc380b4ae05c73a8ef5df926ca1caefe97d
Maksymilian74 <273007@student.pwr.edu.pl>
2023-06-23 00:48:40 +0200

    modyfikacja

--- src/main/java/org/example/Mushroom.java
+++ src/main/java/org/example/Mushroom.java
@@ -6,12 +6,10 @@ package org.example;
 
 public class Mushroom extends Object{
     private int id;
-    private boolean IfCollected;
     private String name;
-    public Mushroom(int position_x, int position_y, int id, boolean IfCollected, String name) {
+    public Mushroom(int position_x, int position_y, int id, String name) {
         super(position_x, position_y);
         this.id = id;
-        this.IfCollected = IfCollected;
         this.name = name;
     }
 
@@ -23,11 +21,4 @@ public class Mushroom extends Object{
         return name;
     }
 
-    public boolean getIfCollected() {
-        return IfCollected;
-    }
-
-    public void setIfCollected() {
-        this.IfCollected = IfCollected;
-    }
 }
